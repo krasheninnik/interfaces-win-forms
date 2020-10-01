58a63202682299132920391358426588ad4e1644
Nikita Krasheninnik <krasheninnik99@gmail.com>
2020-10-02 01:31:33 +0700

    init commit

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.aobutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aobutToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip3.TabIndex = 0;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // aobutToolStripMenuItem
+            // 
+            this.aobutToolStripMenuItem.Name = "aobutToolStripMenuItem";
+            this.aobutToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.aobutToolStripMenuItem.Text = "about";
+            this.aobutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip3);
+            this.MainMenuStrip = this.menuStrip3;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem firstMenuRecordToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem aobutToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About aboutForm = new About();
+            aboutForm.ShowDialog();
+        }
+
+        int x, y;
+        bool isRed = true;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            var pen = new Pen(isRed ? Color.FromName("Red") :
+            Color.FromName("Green"), 3);
+            int l = ClientRectangle.Left;
+            int t = ClientRectangle.Top + menuStrip1.Height;
+            int r = ClientRectangle.Right;
+            int b = ClientRectangle.Bottom;
+            e.Graphics.DrawLine(pen, l, t, r, b);
+            e.Graphics.DrawLine(pen, r, t, l, b);
+            e.Graphics.DrawString($"({x},{y})", DefaultFont,
+            new SolidBrush(Color.Black), x, y);
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button.HasFlag(MouseButtons.Right))
+            {
+                isRed = !isRed;
+                Invalidate();
+            }
+
+        }
+    }
+}
