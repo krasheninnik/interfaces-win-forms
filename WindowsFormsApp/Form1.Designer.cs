3ce3a0879a9f2b38e1abe4ff06683374d4ffcf0b
Nikita Krasheninnik <krasheninnik99@gmail.com>
2020-11-02 08:30:50 +0700

    add redraw with several files

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsAppDataGrid
 {
     partial class Form1
     {
@@ -28,50 +30,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
-            this.aobutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip3.SuspendLayout();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.failToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip3
+            // dataGridView1
             // 
-            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aobutToolStripMenuItem});
-            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip3.Name = "menuStrip3";
-            this.menuStrip3.Size = new System.Drawing.Size(284, 24);
-            this.menuStrip3.TabIndex = 0;
-            this.menuStrip3.Text = "menuStrip3";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(325, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
             // 
-            // aobutToolStripMenuItem
+            // addButton
             // 
-            this.aobutToolStripMenuItem.Name = "aobutToolStripMenuItem";
-            this.aobutToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.aobutToolStripMenuItem.Text = "about";
-            this.aobutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            this.addButton.Location = new System.Drawing.Point(486, 213);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 27);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueMember = "X";
+            series1.YValueMembers = "Y";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Draw";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "as spline",
+            "as lines"});
+            this.comboBox2.Location = new System.Drawing.Point(446, 242);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(115, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox2_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.failToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(716, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // failToolStripMenuItem
+            // 
+            this.failToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.failToolStripMenuItem.Name = "failToolStripMenuItem";
+            this.failToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.failToolStripMenuItem.Text = "Files";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.loadToolStripMenuItem.Text = "open";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(571, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(130, 150);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.SelectionChanged += new System.EventHandler(this.dataGridView2_SelectionChanged);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.menuStrip3);
-            this.MainMenuStrip = this.menuStrip3;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 471);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.menuStrip3.ResumeLayout(false);
-            this.menuStrip3.PerformLayout();
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.MenuStrip menuStrip2;
-        private System.Windows.Forms.ToolStripMenuItem firstMenuRecordToolStripMenuItem;
-        private System.Windows.Forms.MenuStrip menuStrip3;
-        private System.Windows.Forms.ToolStripMenuItem aobutToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button3;
+        private ComboBox comboBox2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem failToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private DataGridView dataGridView2;
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -3,67 +3,253 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
-namespace WindowsFormsApp
+namespace WindowsFormsAppDataGrid
 {
     public partial class Form1 : Form
     {
+        public int selectedChart = -1;
+        public List<Chart> Charts { get; set; }
+        public BindingSource Data { get; set; }
         public Form1()
         {
+            Charts = new List<Chart>();
+            Chart chart = new Chart(name: "default");
+            chart.addPoint(new Data() { X = 0, Y = 0 });
+            chart.addPoint(new Data() { X = 1, Y = 1 });
+            chart.addPoint(new Data() { X = 2, Y = 4 });
+
             InitializeComponent();
+            // set default value for draw mode combobox
+            comboBox2.SelectedItem = "as lines";
+            Charts.Add(chart);
+            selectedChart = 0;
+            dataGridView1.DataSource = Charts[selectedChart].data;
+            dataGridView2.DataSource = GetFileNames();
         }
-
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        private void AddButton_Click(object sender, EventArgs e)
         {
-            About aboutForm = new About();
-            aboutForm.ShowDialog();
-        }
-
-        int x, y;
-        bool isRed = true;
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            e.Graphics.Clear(Color.White);
-            var pen = new Pen(isRed ? Color.FromName("Red") :
-            Color.FromName("Green"), 3);
-            int l = ClientRectangle.Left;
-            int t = ClientRectangle.Top + menuStrip1.Height;
-            int r = ClientRectangle.Right;
-            int b = ClientRectangle.Bottom;
-            e.Graphics.DrawLine(pen, l, t, r, b);
-            e.Graphics.DrawLine(pen, r, t, l, b);
-            e.Graphics.DrawString($"({x},{y})", DefaultFont,
-            new SolidBrush(Color.Black), x, y);
-        }
-        private void Form1_SizeChanged(object sender, EventArgs e)
-        {
-            Invalidate();
-        }
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            x = e.X;
-            y = e.Y;
-            Invalidate();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            Charts[selectedChart].addPoint(new Data() { X = 4, Y = 16 });
         }
 
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void DrawAsLines_Click(object sender, EventArgs e)
         {
-            if (e.Button.HasFlag(MouseButtons.Right))
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+            System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.DataSource = Charts[selectedChart].data;
+        }
+        private void DrawAsSpline_Click(object sender, EventArgs e)
+        {
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+            System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.DataSource = Charts[selectedChart].data;
+        }
+
+        private void drawСhart()
+        {
+            if (chart1 == null) return;
+
+            chart1.DataSource = null;
+
+            // get draw mode:
+            string mode = this.comboBox2.GetItemText(this.comboBox2.SelectedItem);
+            switch (mode)
             {
-                isRed = !isRed;
-                Invalidate();
+                case "as lines":
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    break;
+                case "as spline":
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                    break;
+                default:
+                    MessageBox.Show("Please select draw mode");
+                    break;
             }
 
+            if (selectedChart != -1) chart1.DataSource = Charts[selectedChart].data;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            drawСhart();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drawСhart();
+        }
+
+        private void comboBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //// disable text editing
+            e.Handled = true;
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            drawСhart();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            string[] s = (openFileDialog.FileName.ToString()).Split('\\');
+                            int count = s.Length;
+                            string FileName = s[count - 1];
+
+                            Chart newChart = new Chart(FileName);
+                            fileContent = reader.ReadToEnd();
+                            var lines = fileContent.Split('\n');
+                            foreach (var line in lines)
+                            {
+                                var values = Regex.Split(line, @"\s");
+                                if (values.Length >= 2) // some sort of check of correct of lengts
+                                {
+                                    float x = float.Parse(values[0]);
+                                    float y = float.Parse(values[1]);
+                                    newChart.addPoint(new Data() { X = x, Y = y });
+                                }
+                            }
+                            Charts.Add(newChart);
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show($"File haven't been added. Exception caught: {exception.Message}");
+                    }
+                }
+            }
+
+            // redraw sorces
+            dataGridView1.DataSource = Charts[selectedChart].data;
+            dataGridView2.DataSource = GetFileNames();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    using (StreamWriter writer = new StreamWriter(myStream))
+                    {
+                        // there mehod to extract data from table. oops
+                        var data = "Hello from program, how are you?";
+                        writer.Write(data);
+                    }
+                    myStream.Close();
+                }
+            }
+        }
+
+        public class FileName {
+            public string Name { get; set; }
+            public FileName(string name) { Name = name; }
+        }
+
+        public List<FileName> GetFileNames()
+        {
+         return  Charts.Select(c => new FileName(c.fileName)).ToList();
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView2 != null && dataGridView2.SelectedRows.Count > 0)
+            {
+                selectedChart = dataGridView2.SelectedRows[0].Index;
+                dataGridView1.DataSource = Charts[selectedChart].data;
+            }
         }
     }
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public enum DrawMode
+    {
+        Spline,
+        Lines,
+    }
+
+
+
+    public class Chart
+    {
+        static int id = 0;
+
+        Color getColorById(int id)
+        {
+            Color color;
+            switch (id)
+            {
+                case 0: color = Color.Red; break;
+                case 1: color = Color.Navy; break;
+                case 2: color = Color.Blue; break;
+                case 3: color = Color.Orange; break;
+                default: color = Color.Blue; break;
+            }
+            return color;
+        }
+
+        public Chart(string name)
+        {
+            data = new BindingSource();
+            drawMode = DrawMode.Lines;
+            color = getColorById(id++); // increase id with creating new Chart
+            fileName = name; 
+        }
+
+        public BindingSource data { get; set; }
+        public DrawMode drawMode { get; set; }
+        public Color color { get; set; }
+        public string fileName {get; set; }
+
+        public void addPoint(Data p)
+        {
+            data.Add(p);
+        }
+    }
+
 }
